913a21eadc35b7d096e05e82630f114861369ba4
qiaoyuqiaoyu <Yu@Yus-MacBook-Pro.local>
2015-09-02 22:13:01 -0400

    store constants
    Map type to WoodItem using HashMap.
    Create input file for test

--- src/controller/HardwoodSeller.java
+++ src/controller/HardwoodSeller.java
@@ -2,7 +2,11 @@
  * 
  */
 package controller;
-
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import model.WoodItem;
+import java.util.HashMap;
 /**
  * @author Esteban
  *
@@ -12,13 +16,28 @@ public class HardwoodSeller {
 	/**
 	 * @param args
 	 */
+    HashMap<String, WoodItem> hm;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        HardwoodSeller hs = new HardwoodSeller();
 	}
 	
+    public HardwoodSeller(){
+        hm = new HashMap<>();
+        hm.put("Cherry", new WoodItem("Cherry", 2.5, 5.95));
+        hm.put("Curly Maple", new WoodItem("Curly Maple", 1.5, 6.0));
+        hm.put("Genuine Mahogany", new WoodItem("Genuine Mahogany", 3.0, 9.6));
+        hm.put("Wenge", new WoodItem("Wenge", 5.0, 22.35));
+        hm.put("White Oak", new WoodItem("White Oak", 2.3, 6.7));
+        hm.put("Sawdust", new WoodItem("Sawdust", 1.0, 1.5));
+    }
+    
 	public void readInputFile(String inputFilePath){
 		
+        
+        
+        
+        
 	}
 	
 	public Double deliveryTime(){
